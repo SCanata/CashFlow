44ad2ff5bae00fae030ffac83e0b18d4b6e9516c
SCanata <42221654+SCanata@users.noreply.github.com>
2025-06-06 12:54:37 -0400

    Improve CLI output

---
+++ cashflow/__init__.py
@@ -0,0 +1,27 @@
+"""CashFlow game engine package."""
+
+from .engine import (
+    Game,
+    Board,
+    Player,
+    SpaceType,
+    Asset,
+    Liability,
+    DealCard,
+    DoodadCard,
+    MarketCard,
+    Deck,
+)
+
+__all__ = [
+    "Game",
+    "Board",
+    "Player",
+    "SpaceType",
+    "Asset",
+    "Liability",
+    "DealCard",
+    "DoodadCard",
+    "MarketCard",
+    "Deck",
+]
